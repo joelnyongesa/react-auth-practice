b4a61af0948673f589ad33089b7d30e8e2902e42
joelnyongesa <joelnyongesa148@gmail.com>
2023-12-26 12:59:25 +0300

    feat: added the sign up and log in routes

---
+++ back-end/src/routes/index.js
@@ -0,0 +1,9 @@
+import { testRoute } from "./testRoute";
+import { signUpRoute } from "./signUpRoute";
+import { loginRoute } from "./loginroute";
+
+export const routes = [
+    testRoute,
+    signUpRoute,
+    loginRoute
+]
